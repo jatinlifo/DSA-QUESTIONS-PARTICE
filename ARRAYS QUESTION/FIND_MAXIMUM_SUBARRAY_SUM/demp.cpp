beb0101fdb8c8ec8483b8cb7c7d796ec3bcb3066
jatinlifo <jatinstudentjatin@gmail.com>
2024-07-17 17:02:08 +0530

    Add new file

---
+++ ARRAYS QUESTION/FIND_MAXIMUM_SUBARRAY_SUM/demp.cpp
@@ -0,0 +1,50 @@
+// Given an integer array arr[]. Find the contiguous sub-array(containing at least one number) that has the maximum sum and return its sum.
+
+// Examples:
+// Input: arr[] = [1, 2, 3, -2, 5]
+// Output: 9
+// Explanation: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    public:
+
+     long long maxSubarraySum(vector<int> &arr) {
+       
+       int resFirst = arr[0];
+       int maxSum  = arr[0];
+       
+       for(int i=1;i<arr.size();i++)
+       {
+           resFirst = max(resFirst + arr[i] , arr[i]);
+           if(resFirst > maxSum){
+               maxSum = resFirst;
+           }
+       }
+       return maxSum;
+    }
+};
+
+int main ()
+{
+    int n;
+    cout<<"Enter the array size;";
+    cin>>n;
+    vector<int> v(n);
+    cout<<"Enter the elemets:";
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+
+    Solution obj;
+
+    long long  int ans = obj.maxSubarraySum(v);
+    
+    cout<<"Maximum Subarray sum: "<<ans<<endl;
+    return 0;
+}
---
+++ ARRAYS QUESTION/MINIIMIZE_THE_HEIGHTS/demo.cpp
@@ -0,0 +1,53 @@
+// // Given an array arr[] denoting heights of N towers and a positive integer K.
+// // For each tower, you must perform exactly one of the following operations exactly once.
+// // Increase the height of the tower by K
+// // Decrease the height of the tower by K
+// // Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+
+// Input:
+// K = 2, N = 4 Arr[] = {1, 5, 8, 10}
+// Output: 5 // Explanation: {1+k, 5-k, 8-k, 10-k} = {3, 3, 6, 8}. 
+// The difference smallest is 8-3 = 5.
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int MinDiff(vector<int> &arr, int n, int k) {
+
+        sort(arr.begin(),arr.end());
+        int mini = arr[0];
+        int maxi = arr[n-1];
+        int res = maxi - mini;
+        
+        for(int i=1; i<n; i++)
+        {
+            maxi = max(arr[i-1] +k, arr[n-1] - k); //if kya last or minimize ho sakta hai
+            mini = min(arr[i] -k, arr[0] + k);  // if start sa or bada kar sakta hai 
+            if(mini < 0) continue;
+            res = min(res, maxi - mini);   // check kon minimum hai
+        }
+        return res;
+    }
+
+int main ()
+{
+    int n;
+    cout<<"Enter the tower size:";
+    cin>>n;
+    vector<int> v(n);
+    cout<<"Enter element:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    int k;
+    cout<<"Enter k:";
+    cin>>k;
+
+    int ans = MinDiff(v,n,k) ;
+    cout<<"Minimum is : "<<ans;
+
+    return 0;
+}
